b373e6221580adeba917b55f7c9d7c232fc0f158
Ali Essam <aliduku@gmail.com>
2023-04-14 13:16:10 +0200

    Create Data Manipulation Language Checkpoint.sql

---
+++ Checkpoint/Data Manipulation Language Checkpoint.sql
@@ -0,0 +1,14 @@
+INSERT INTO customers VALUES (1, 'Alice', 'alice@example.com', '123 Main St.');
+INSERT INTO customers VALUES (2, 'Bob', 'bob@example.com', '456 Market St.');
+INSERT INTO customers VALUES (3, 'Charlie', 'charlie@example.com', '789 Elm St.');
+
+INSERT INTO products VALUES (1, 'Widget', 10.00);
+INSERT INTO products VALUES (2, 'Gadget', 20.00);
+INSERT INTO products VALUES (3, 'Doohickey', 15.00);
+
+INSERT INTO orders VALUES (1, 1, 1, 10, '2021-01-01');
+INSERT INTO orders VALUES (2, 1, 2, 5, '2021-01-02');
+INSERT INTO orders VALUES (3, 2, 1, 3, '2021-01-03');
+INSERT INTO orders VALUES (4, 2, 2, 7, '2021-01-04');
+INSERT INTO orders VALUES (5, 3, 1, 2, '2021-01-05');
+INSERT INTO orders VALUES (6, 3, 3, 3, '2021-01-06');
